3fe0e379d3079a662c0d08eff294ce5536655059
stoilsteve-hub <stoil.steve@me.com>
2025-12-19 18:36:08 +0100

    Added: initial model structure with JobSeeker - Added JobSeeker class with internal UUID id - Verified model with simple Main sanity test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.EducationLevel;
+import model.JobSeeker;
+
 public class Main {
     public static void main(String[] args) {
+
+        //just to test the logic in JobSeeker so far
+        JobSeeker seeker = new JobSeeker("Super Man", EducationLevel.BACHELOR, 3, "IT");
+        System.out.println(seeker);
     }
 }
--- src/model/JobSeeker.java
+++ src/model/JobSeeker.java
@@ -1,4 +1,46 @@
 package model;
 
+import java.util.UUID;
+
 public class JobSeeker {
+    private final UUID id;
+    private String fullName;
+    private EducationLevel education;
+    private int yearsOfExperience;
+    private String workArea;
+
+    public JobSeeker( String fullName, EducationLevel education, int yearsOfExperience, String workArea) {
+        this.id = UUID.randomUUID();
+        this.fullName = fullName;
+        this.education = education;
+        this.yearsOfExperience = yearsOfExperience;
+        this.workArea = workArea;
+
+    }
+    public UUID getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public EducationLevel getEducation() {
+        return education;
+    }
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+    public String getWorkArea() {
+        return workArea;
+    }
+    @Override
+    public String toString() {
+        return "JobSeeker{"+
+                "id="+ id +
+                ",fullName=" + fullName + '\'' +
+                ", education=" + education +
+                ", yearsExperience=" + yearsOfExperience  +
+                ", workArea='" + workArea + '\'' +
+                '}';
+
+    }
 }
