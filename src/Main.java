7d4e2aab1c8855e8f43c7d4f99e9ed22c872b154
stoilsteve-hub <stoil.steve@me.com>
2025-12-19 18:48:02 +0100

    Added: JobOpening model class - Implemented JobOpening with internal UUID id - Added required education level, minimum experience and work area fields - Verified creation and output via Main test

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import model.EducationLevel;
+import model.JobOpening;
 import model.JobSeeker;
 
 public class Main {
     public static void main(String[] args) {
-
-        //just to test the logic in JobSeeker so far
         JobSeeker seeker = new JobSeeker("Super Man", EducationLevel.BACHELOR, 3, "IT");
+        JobOpening opening = new JobOpening("Super Woman", EducationLevel.HIGH_SCHOOL, 0, "Hospitality");
+
         System.out.println(seeker);
+        System.out.println(opening);
     }
 }
--- src/model/JobOpening.java
+++ src/model/JobOpening.java
@@ -1,4 +1,46 @@
 package model;
 
+import java.util.UUID;
+
 public class JobOpening {
+    private final UUID id;
+    private String title;
+    private EducationLevel requiredEducation;
+    private int minYearsExperience;
+    private String workArea;
+
+    public JobOpening( String title, EducationLevel requiredEducation, int minYearsExperience, String workArea) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.requiredEducation = requiredEducation;
+        this.minYearsExperience = minYearsExperience;
+        this.workArea = workArea;
+
+    }
+    public UUID getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public EducationLevel getRequiredEducation() {
+        return requiredEducation;
+    }
+    public int getMinYearsExperience() {
+        return minYearsExperience;
+    }
+    public String getWorkArea() {
+        return workArea;
+    }
+    @Override
+    public String toString() {
+        return "JobOpening{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", requiredEducation=" + requiredEducation +
+                ", minYearsExperience=" + minYearsExperience +
+                ", workArea='" + workArea + '\'' +
+                '}';
+    }
+
 }
