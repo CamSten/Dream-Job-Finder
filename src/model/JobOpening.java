5d08a5491e99c48b071dc2de15f84118e718efc5
stoilsteve-hub <stoil.steve@me.com>
2025-12-29 11:49:57 +0100

    updated JobSeeker and JobOpening with equals and file helper methods

--- src/model/JobOpening.java
+++ src/model/JobOpening.java
@@ -9,7 +9,7 @@ public class JobOpening {
     private int minYearsExperience;
     private String workArea;
 
-    public JobOpening( String title, EducationLevel requiredEducation, int minYearsExperience, String workArea) {
+    public JobOpening(String title, EducationLevel requiredEducation, int minYearsExperience, String workArea) {
         this.id = UUID.randomUUID();
         this.title = title;
         this.requiredEducation = requiredEducation;
@@ -17,21 +17,44 @@ public class JobOpening {
         this.workArea = workArea;
 
     }
+
     public UUID getId() {
         return id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public EducationLevel getRequiredEducation() {
         return requiredEducation;
     }
+
     public int getMinYearsExperience() {
         return minYearsExperience;
     }
+
     public String getWorkArea() {
         return workArea;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        JobOpening that = (JobOpening) o;
+
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
     @Override
     public String toString() {
         return "JobOpening{" +
@@ -43,4 +66,29 @@ public class JobOpening {
                 '}';
     }
 
+    // --- Helper methods for Text File Storage ---
+
+    public String toDataString() {
+        return id + ";" + title + ";" + requiredEducation + ";" + minYearsExperience + ";" + workArea;
+    }
+
+    public static JobOpening fromDataString(String line) {
+        String[] parts = line.split(";");
+        UUID id = UUID.fromString(parts[0]);
+        String title = parts[1];
+        EducationLevel edu = EducationLevel.valueOf(parts[2]);
+        int years = Integer.parseInt(parts[3]);
+        String area = parts[4];
+
+        return new JobOpening(id, title, edu, years, area);
+    }
+
+    private JobOpening(UUID id, String title, EducationLevel requiredEducation, int minYearsExperience,
+            String workArea) {
+        this.id = id;
+        this.title = title;
+        this.requiredEducation = requiredEducation;
+        this.minYearsExperience = minYearsExperience;
+        this.workArea = workArea;
+    }
 }
--- src/model/JobSeeker.java
+++ src/model/JobSeeker.java
@@ -33,14 +33,66 @@ public class JobSeeker {
         return workArea;
     }
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JobSeeker jobSeeker = (JobSeeker) o;
+
+        return id.equals(jobSeeker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
     public String toString() {
-        return "JobSeeker{"+
-                "id="+ id +
-                ",fullName=" + fullName + '\'' +
+        return "JobSeeker{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
                 ", education=" + education +
-                ", yearsExperience=" + yearsOfExperience  +
+                ", yearsOfExperience=" + yearsOfExperience +
                 ", workArea='" + workArea + '\'' +
                 '}';
+    }
 
+    // --- Helper methods for Text File Storage ---
+
+    /**
+     * Converts this object to a semicolon-separated string (e.g., "id;Name;DEGREE;years;Area")
+     */
+    public String toDataString() {
+        return id + ";" + fullName + ";" + education + ";" + yearsOfExperience + ";" + workArea;
+    }
+
+    /**
+     * Creates a JobSeeker from a semicolon-separated string line.
+     */
+    public static JobSeeker fromDataString(String line) {
+        String[] parts = line.split(";");
+        // Expected format: id;fullName;education;years;workArea
+        UUID id = UUID.fromString(parts[0]);
+        String name = parts[1];
+        EducationLevel edu = EducationLevel.valueOf(parts[2]);
+        int years = Integer.parseInt(parts[3]);
+        String area = parts[4];
+
+        // Reconstruct using the existing constructor
+        JobSeeker seeker = new JobSeeker(name, edu, years, area);
+        // Reflection or a protected setter would be needed to restore the exact UUID,
+        // but for now, since the constructor generates a random UUID, we might need a constructor that accepts UUID
+        // to fully restore state. Let's add a constructor for restoring state.
+        return new JobSeeker(id, name, edu, years, area);
+    }
+
+    // Private constructor for restoring from file with existing ID
+    private JobSeeker(UUID id, String fullName, EducationLevel education, int yearsOfExperience, String workArea) {
+        this.id = id;
+        this.fullName = fullName;
+        this.education = education;
+        this.yearsOfExperience = yearsOfExperience;
+        this.workArea = workArea;
     }
 }
